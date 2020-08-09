a812f9e1087ec502d527a45f8c738b937d2b4473
myst-0123 <52592597+myst-0123@users.noreply.github.com>
2020-08-09 21:37:33 +0900

    add EventListener

--- VstInstrument/VstProcessor.cpp
+++ VstInstrument/VstProcessor.cpp
@@ -12,8 +12,10 @@ namespace Vst {
     {
         tresult result = AudioEffect::initialize(context);
         if (result == kResultTrue) {
-            addAudioInput(STR16("AudioInput"), SpeakerArr::kStereo);
+            addEventInput(STR16("MIDI Input"), 1);
             addAudioOutput(STR16("AudioOutput"), SpeakerArr::kStereo);
+        
+            pitchList.clear();
         }
 
         return result;
@@ -29,17 +31,76 @@ namespace Vst {
 
     tresult PLUGIN_API VstProcessor::process(ProcessData& data)
     {
-        Sample32* inL = data.inputs[0].channelBuffers32[0];
-        Sample32* inR = data.inputs[0].channelBuffers32[1];
+        IEventList* eventList = data.inputEvents;
+        if (eventList != NULL) {
+            int32 numEvent = eventList->getEventCount();
+            for (int32 i = 0; i < numEvent; i++) {
+                Event event;
+                if (eventList->getEvent(i, event) == kResultOk) {
+                    int16 channel;
+                    int16 noteNo;
+                    int16 velocity;
+
+                    switch (event.type) {
+                    case Event::kNoteOnEvent:
+                        channel = event.noteOn.channel;
+                        noteNo = event.noteOn.pitch;
+                        velocity = event.noteOn.velocity;
+
+                        onNoteOn(channel, noteNo, velocity);
+
+                        break;
+                    case Event::kNoteOffEvent:
+                        channel = event.noteOff.channel;
+                        noteNo = event.noteOff.pitch;
+                        velocity = event.noteOff.velocity;
+
+                        onNoteOff(channel, noteNo, velocity);
+
+                        break;
+                    }
+                }
+            }
+        }
+
         Sample32* outL = data.outputs[0].channelBuffers32[0];
         Sample32* outR = data.outputs[0].channelBuffers32[1];
 
         for (int32 i = 0; i < data.numSamples; i++) {
-            outL[i] = inL[i];
-            outR[i] = outR[i];
+            float pitch = 0.0f;
+
+            if (!pitchList.empty())
+                pitch = pitchList.back();
+
+
+            outL[i] = makeSound(pitch);
+            outR[i] = makeSound(pitch);
         }
 
         return kResultTrue;
+    }
+
+    void VstProcessor::onNoteOn(int channel, int note, float velocity)
+    {
+        float pitch = (440.0f * powf(2.0f, (float)(note - (69)) / 12.0f));
+        pitchList.push_back(pitch);
+    }
+
+    void VstProcessor::onNoteOff(int channel, int note, float velocity)
+    {
+        float pitch = (440.0f * powf(2.0f, (float)(note - (69)) / 12.0f));
+
+        auto pitchListItr = find(pitchList.begin(), pitchList.end(), pitch);
+        
+        if (pitchListItr != pitchList.end()) {
+            pitchList.erase(pitchListItr);
+        }
+
+    }
+
+    Sample32 VstProcessor::makeSound(float pitch)
+    {
+
     }
 
 } }
